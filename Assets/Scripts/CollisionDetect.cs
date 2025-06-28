be299339192bda969bafdbb37e6ae77adc60a625
Ryunite <lazyryu12@gmail.com>
2025-06-28 15:58:44 +0700

    Add MainMenu

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionDetect : MonoBehaviour
 {
@@ -22,5 +23,7 @@ public class CollisionDetect : MonoBehaviour
         mainCam.GetComponent<Animator>().Play("CollisionCam");
         yield return new WaitForSeconds(3);
         fadeOut.SetActive(true);
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(0);
     }
 }
