a6a9b72bc94ceb7447dff5294a7d2223c3b6aa8d
Ryunite <lazyryu12@gmail.com>
2025-06-28 15:36:10 +0700

    Add CollisionsFX & FadeOut

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -6,9 +6,21 @@ public class CollisionDetect : MonoBehaviour
 {
     [SerializeField] GameObject thePlayer;
     [SerializeField] GameObject playerAnim;
+    [SerializeField] AudioSource collisionFX;
+    [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject fadeOut;
     void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(CollisionEnd());
+    }
+
+    IEnumerator CollisionEnd()
+    {
+        collisionFX.Play();
         thePlayer.GetComponent<PlayerMovement>().enabled = false;
         playerAnim.GetComponent<Animator>().Play("Stumble Backwards");
+        mainCam.GetComponent<Animator>().Play("CollisionCam");
+        yield return new WaitForSeconds(3);
+        fadeOut.SetActive(true);
     }
 }
